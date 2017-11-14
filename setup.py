d2b2a79dc590bd5759fc8b7d47d9cd5be65254a0
Igor Brites <igor.brites@maxmilhas.com.br>
2017-11-13 22:23:48 -0200

    Package start

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='SSH-It',
+    packages=['src'],
+    version='0.0.0',
+    description='Simple SSH CLI',
+    author='Igor Brites',
+    author_email='igor.brites.87@gmail.com',
+    url='https://github.com/igorbrites/sshit',
+    download_url='',
+    keywords=['ssh', 'cli'],
+    classifiers=[]
+)
